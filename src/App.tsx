70a0d04ae7e87f87a56cce8664f012c3fca97f29
Yuri Cianci <yucianci@gmail.com>
2022-10-20 20:10:16 -0300

    :heavy_plus_sign: style: added eslint and prettier dev dependecies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function App() {
-
   return (
     <div className="App">
       <h1>Challenge - Coderockr</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
